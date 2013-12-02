a9fe02e26005e831b805e11876a4fa16bb802514
Jason Stenhouse <jstenhouse@newrelic.com>
2013-12-02 10:16:51 -0800

    making verify java/ruby run at execution time instead of compile time

--- definitions/verify_java.rb
+++ definitions/verify_java.rb
@@ -1,7 +1,14 @@
 define :verify_java do
-  # check java dependency
-  unless node[:languages][:java] && node[:languages][:java][:version].start_with?('1.6', '1.7')
-    Chef::Application.fatal!("The New Relic #{params[:name]} requires a Java version >= 1.6 -" +
-      " For more information, see https://docs.newrelic.com/docs/plugins/installing-a-plugin")
+  ohai "reload" do
+    action :reload
+  end
+  ruby_block "verify java for #{params[:name]}" do
+    block do
+      # check java dependency
+      unless node[:languages][:java] && node[:languages][:java][:version].start_with?('1.6', '1.7')
+        Chef::Application.fatal!("The New Relic #{params[:name]} requires a Java version >= 1.6 -" +
+          " For more information, see https://docs.newrelic.com/docs/plugins/installing-a-plugin")
+      end
+    end
   end
 end
